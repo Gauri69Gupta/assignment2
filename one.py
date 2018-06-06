222b8c760b7cc57e4cf6fb96f7d63f450f551b41
Gauri69Gupta <gauri.gupta166@gmail.com>
2018-06-06 22:21:07 +0530

    my second change

---
+++ one.py
@@ -0,0 +1,25 @@
+QUES-1
+print("hello! Have a nice day")
+***************************************************************
+QUES-2
+a="Acad"
+b="View"
+print(a+b)
+***************************************************************
+QUES-3
+x=input("Enter value of x")
+y=input("Enter value of y")
+z=input("Enter value of z")
+print(x,y,z,sep='--')
+***************************************************************
+QUES-4
+print('''"Let's get started"''')
+***************************************************************
+QUES-5
+print('''s="Acadview"\ncourse="Python"\nfees="5000"''')
+***************************************************************
+QUES-6
+name=('''"Tony Stark"''')
+salary=1000000
+print("%s is name and %d is salary" %(name,salary))
+**************************************************************
